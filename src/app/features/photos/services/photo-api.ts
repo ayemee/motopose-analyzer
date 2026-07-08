a788dd5394c40ce607087893734bb1e0bbc5d546
ayemee <aimee.o.ardines@gmail.com>
2026-07-08 21:32:41 +0800

    update call to endpoint

--- src/app/features/photos/services/photo-api.ts
+++ src/app/features/photos/services/photo-api.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from '@angular/core';
 import { UploadPhotoRequest } from '../models/upload-photo-request.model';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoApiService {
   private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/photos`;
   uploadPhoto(request: UploadPhotoRequest) {
     const formData = new FormData();
 
@@ -14,6 +16,6 @@ export class PhotoApiService {
     formData.append('riderName', request.riderName);
     formData.append('notes', request.notes);
 
-    return this.http.post('/api/photos', formData);
+    return this.http.post(this.apiUrl, formData);
   }
 }
